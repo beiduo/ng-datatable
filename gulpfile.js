8b9fc2564522bf93475e4c769d1544498f4ff9ba
Vince Snow <mrussel@163.com>
2017-01-13 16:01:32 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -11,10 +11,12 @@ var uglify = require("gulp-uglify");
 var ngHtml2Js = require('gulp-ng-html2js');
 var minifyHtml = require("gulp-minify-html");
 
+require('gulp-awaitable-tasks')(gulp);
+
 
 // 合并，压缩文件
-gulp.task('build', function () {
-    gulp.src("./src/templates/**/*.html")
+gulp.task('build', function* () {
+    yield gulp.src("./src/templates/**/*.html")
         .pipe(minifyHtml({
             empty: true,
             spare: true,
@@ -27,15 +29,15 @@ gulp.task('build', function () {
         .pipe(concat("template.js"))
         .pipe(gulp.dest("./src"));
         
-    gulp.src('./src/index.js')
+    yield gulp.src('./src/index.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(webpack(require('./webpack.config.js')))
         .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('demo', function () {
-    gulp.src('./demo/src/index.js')
+gulp.task('demo', function* () {
+    yield gulp.src('./demo/src/index.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'))
         .pipe(webpack(require('./demo/webpack.config.js')))
